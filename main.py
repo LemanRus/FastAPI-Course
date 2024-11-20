db79da7d5533dd78977ec8145d09ccddf280ab1f
LemanRus <wlad163163@gmail.com>
2024-11-20 23:44:08 +0300

    Do calculate numbers exercise

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return FileResponse("index.html")
+
+
+@app.post("/calculate")
+async def calc(num_a: float, num_b: float):
+    return {"result": num_a + num_b}
