b49dfc9cf1902007a54b868478641fc90eca9f2a
LemanRus <wlad163163@gmail.com>
2024-11-22 23:50:39 +0300

    Pydantic model exercise

--- app/main.py
+++ app/main.py
@@ -1,14 +1,22 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
+from models.models import User
+
 app = FastAPI()
 
+my_user: User = User(name="Stephen Hawking", id=1)
 
 @app.get("/")
 async def root():
-    return FileResponse("../index.html")
+    return FileResponse("./app/index.html")
 
 
 @app.post("/calculate")
 async def calc(num_a: float, num_b: float):
     return {"result": num_a + num_b}
+
+
+@app.get("/users")
+async def show_user():
+    return my_user
