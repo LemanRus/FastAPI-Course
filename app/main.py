218c6cd4c4dbe2bbfaf7966a3448c9247d545da3
LemanRus <wlad163163@gmail.com>
2024-11-23 22:58:44 +0300

    Pydantic model exercise - "is_adult"

--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@ from models.models import User
 
 app = FastAPI()
 
-my_user: User = User(name="Stephen Hawking", id=1)
 
 @app.get("/")
 async def root():
@@ -17,6 +16,8 @@ async def calc(num_a: float, num_b: float):
     return {"result": num_a + num_b}
 
 
-@app.get("/users")
-async def show_user():
-    return my_user
+@app.post("/user")
+async def check_adult(user: User) -> User:
+    if user.age >= 18:
+        user.is_adult = True
+    return user
