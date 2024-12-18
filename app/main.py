b135c315bcb9eedeb88d0b3d4ed05f35ef9cc30e
LemanRus <wlad163163@gmail.com>
2024-12-18 23:46:17 +0400

    Create user exercise

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 
-from models.models import User
+from models.models import User, UserCreate
 
 app = FastAPI()
-
+mock_db: list[UserCreate] = []
 
 @app.get("/")
 async def root():
@@ -20,4 +20,15 @@ async def calc(num_a: float, num_b: float):
 async def check_adult(user: User) -> User:
     if user.age >= 18:
         user.is_adult = True
+    return user
+
+
+@app.get("/show_users")
+async def show_users():
+    return {"users": mock_db}
+
+
+@app.post("/user_create")
+async def create_user(user: UserCreate) -> UserCreate:
+    mock_db.append(user)
     return user
--- models/models.py
+++ models/models.py
@@ -1,6 +1,14 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PositiveInt, Field, EmailStr
+
 
 class User(BaseModel):
     name: str
     age: int
     is_adult: bool = False
+
+
+class UserCreate(BaseModel):
+    name: str
+    email: EmailStr
+    age: PositiveInt | None = Field(default=None, lt=130)
+    is_subscribed: bool = False
